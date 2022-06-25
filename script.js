e9e06386ab6d5588849486f70bef36008e307d3f
Seby <107957844+Seby7890@users.noreply.github.com>
2022-06-25 07:01:26 +0000

    comment function

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let inputField = document.getElementById("input")
 let messagesContainer = document.getElementById("messages");
 inputField.addEventListener("keypress", getInputValue)
-
 let scanner;
 let hobbyNumber = 0
 let hobbies = []
@@ -12,11 +11,8 @@ function getInputValue(event) {
   if (event.key === "Enter") {
     let inputValue = inputField.value;
     inputField.value = ""
-    
     addUserEntry(inputValue)
-    
     performSelectedAction(inputValue)
-    
     let menu = getMenuString()
     addBotEntry(menu)
   }
@@ -36,29 +32,29 @@ function addBotEntry(input) {
   messagesContainer.appendChild(botMessageDiv);
 }
 
-function talkAboutHobbies() {
-  let option
-  do {
-    printMenu()
-    console.log('Choose option: ')
-    option = scanner.questionInt()
-    performSelectedAction(option)
-  } while (option != 5)
-}
+// function talkAboutHobbies() {
+//   let option
+//   do {
+//     printMenu()
+//     console.log('Choose option: ')
+//     option = scanner.questionInt()
+//     performSelectedAction(option)
+//   } while (option != 5)
+// }
 
 function getMenuString() {
-  let menu='Hi there, I am your assistant. How can I help you?'+
-  '\n1. Add hobby'+
-  '\n2. How much is a hobby'+
-  '\n3. Which is the cheapest hobby'+
-  '\n4. Erase hobby'+
-  '\n5. Recommend a hobby for today'+
-  '\n6. No more hobbies';
+  let menu = 'Hi there, I am your assistant. How can I help you?' +
+    '\n1. Add hobby' +
+    '\n2. How much is a hobby' +
+    '\n3. Which is the cheapest hobby' +
+    '\n4. Erase hobby' +
+    '\n5. Recommend a hobby for today' +
+    '\n6. No more hobbies';
   return menu;
 }
 
 function handleOption1(input) {
-  let hobbyDetails = input.split(",") //["tenis", "44"]
+  let hobbyDetails = input.split(",")
   let hobbyName = hobbyDetails[0]
   let hobbyPrice = hobbyDetails[1]
   let hobby = {
@@ -66,7 +62,7 @@ function handleOption1(input) {
     price: hobbyPrice
   }
   addHobby(hobby)
-  addBotEntry("Your hobbies are: "+JSON.stringify(hobbies))
+  addBotEntry("Your hobbies are: " + JSON.stringify(hobbies))
 }
 
 function addHobby(hobby) {
@@ -121,10 +117,8 @@ function handleOption5() {
 }
 
 function performSelectedAction(input) {
-  //option = "1:tenis,44"
   let optionDetails = input.split(":")
-  let option = optionDetails[0] //1
-  // optionDetails[1] = "tenis,44"
+  let option = optionDetails[0]
   let hobbyDetails = optionDetails[1]
   switch (Number(option)) {
     case 1:
